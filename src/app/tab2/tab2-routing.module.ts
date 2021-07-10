70558e951c81b1cdc95d180ad937bb1a6af36502
SandovaI <calebsandoval901@gmail.com>
2021-07-10 16:02:33 -0700

    1 file modified, 7 added, 5 deleted

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -5,10 +5,9 @@ import { Tab2Page } from './tab2.page';
 const routes: Routes = [
   {
     path: '',
-    component: Tab2Page,
+    component: Tab2Page
   }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
