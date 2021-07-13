c47a95728ec1b17cbb1b3ca0030fcb1467ef2280
SandovaI <calebsandoval901@gmail.com>
2021-07-13 15:31:13 -0700

    6 files modified

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
-
+  searchText;
+  heroes = [
+    { id: 11, name: 'Mr. Nice', produce: 'India' },
+    { id: 12, name: 'Narco', produce: 'USA' },
+    { id: 13, name: 'Bombasto', produce: 'UK' },
+    { id: 14, name: 'Celeritas', produce: 'Canada' },
+    { id: 15, name: 'Magneta', produce: 'Russia' },
+    { id: 16, name: 'RubberMan', produce: 'China' },
+    { id: 17, name: 'Dynama', produce: 'Germany' },
+    { id: 18, name: 'Dr IQ', produce: 'Hong Kong' },
+    { id: 19, name: 'Magma', produce: 'South Africa' },
+    { id: 20, name: 'Tornado', produce: 'Sri Lanka' }
+  ];
 }
