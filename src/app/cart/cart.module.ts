e46c516b71fb947de20550c5f891c641a3eaec06
SandovaI <calebsandoval901@gmail.com>
2021-07-16 14:56:22 -0700

    2 files modified

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -5,13 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CartPage } from './cart.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { CartPageRoutingModule } from './cart-routing.module.ts';
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    CartPageRoutingModule,
     RouterModule.forChild([{ path: '', component: CartPage }])
   ],
   declarations: [CartPage]
