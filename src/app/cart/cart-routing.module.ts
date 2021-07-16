3faacf4e0403ecda5b3714e5a9f24cab6ab6dfd8
SandovaI <calebsandoval901@gmail.com>
2021-07-16 14:48:56 -0700

    2 files modified, 1 added

--- src/app/cart/cart-routing.module.ts
+++ src/app/cart/cart-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartPage } from './cart.page';
+import { PaymentPage } from '../payment/payment.page';
 
 const routes: Routes = [
   {
     path: '',
     component: CartPage
+  },
+  {
+    path: 'payment',
+    loadChildren: () =>
+      import('../payment/payment.module').then(m => m.PaymentPageModule)
   }
 ];
 
