79e85ede9126ae5d4e738b8240c8d8e815621d5b
SandovaI <calebsandoval901@gmail.com>
2021-07-21 14:33:04 -0700

    2 files modified, 6 added

--- src/app/order/order-routing.module.ts
+++ src/app/order/order-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaymentPage } from './payment.page';
+import { OrderPage } from './order.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: PaymentPage
-  },
-  {
-    path: 'order',
-    loadChildren: () =>
-      import('../order/order.module').then(m => m.OrderPageModule)
+    component: OrderPage
   }
 ];
 
@@ -18,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PaymentPageRoutingModule {}
+export class OrderPageRoutingModule {}
