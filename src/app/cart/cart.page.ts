37c39cb84b856ea4b3f99f4d19970730a5bc9c03
SandovaI <calebsandoval901@gmail.com>
2021-08-06 13:45:40 -0700

    2 files modified

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import { PaymentPage } from '../payment/payment.page';
 @Component({
   selector: 'app-cart',
   templateUrl: 'cart.page.html',
   styleUrls: ['cart.page.scss']
 })
 export class CartPage {
-  constructor() {}
+  constructor(public modalController: ModalController) {}
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: PaymentPage
+    });
+    return await modal.present();
+  }
 }
